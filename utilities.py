5aaf700edfeeefbcc64c4468261a4a1b4841a369
adamelkholyy <78866717+adamelkholyy@users.noreply.github.com>
2024-09-20 15:02:10 +0100

    cleanup utilities.py

--- utilities.py
+++ utilities.py
@@ -1,8 +1,8 @@
-import whisper
 import os 
 import json
-import torchaudio
 
+from torchaudio import load
+from torchaudio.transforms import Resample
 from datasets import Dataset
 from pydub import AudioSegment
 from tqdm import tqdm
@@ -21,43 +21,31 @@ def save_transcript(transcript=None, ds=None, transcript_filename="transcript.tx
     Returns:
         None
     """
+    # check transcript passed in args
     if not transcript and not ds: 
         raise TypeError("[utilities]: No value passed for ds or transcript")
         
     os.makedirs("transcripts", exist_ok=True)
     transcript_path = os.path.join("transcripts", transcript_filename)
+
     with open(transcript_path, "w") as file:
         # save transcript string to file
         if transcript:
             for line in transcript.split("."):
                 file.write(line[1:] + ".\n")
                 if verbose: print(line[1:])
+
         # save transcript from dataset to file
         elif ds: 
             for batch in ds:
                 text = batch["transcription"][1:]
                 file.write(text + "\n")
                 if verbose: print(text)
+
     print(f"[utilities] Transcript successfully written to {transcript_path}")
 
 
-def transcribe_mp3(mp3_path: str, model_type="base"):
-    """
-    Transcribes a given mp3 file using the specified whisper model.
-    Args:
-        mp3_path (str, optional): The path to the mp3 file to be transcribed. Defaults to 'downloads/audio.mp3'
-        model_type (str, optional): The whisper model used to transcribe. Defaults to 'base'
-    Returns:
-        transcription["text"] (str): The whisper transcription of the audio file
-    """
-    whisper_model = whisper.load_model(model_type)
-    print(f"[utilities] whisper-{model_type} loaded, transcribing {mp3_path}. Note: This could take a long time depending on the length of the audio")
-    transcription = whisper_model.transcribe(mp3_path)
-    print(f"[utilities] {mp3_path} successfully transcribed by whisper-{model_type}")
-    return transcription["text"]
-
-
-def convert_audio_for_whisper(batch):
+def resample_audio(batch):
     """
     Converts the audio in the given batch to the correct format for Whisper transcription.
     Args:
@@ -67,29 +55,30 @@ def convert_audio_for_whisper(batch):
     """
     new_sample_rate = 16000
     audio_file = batch["audio"]
-    waveform, old_sample_rate = torchaudio.load(audio_file)
+    waveform, old_sample_rate = load(audio_file)
 
     # resample to 16khz
-    resampler = torchaudio.transforms.Resample(orig_freq=old_sample_rate, new_freq=new_sample_rate)
+    resampler = Resample(orig_freq=old_sample_rate, new_freq=new_sample_rate)
     waveform = resampler(waveform)
 
     # convert to numpy array
     batch["audio"] = {
-        "array": waveform.squeeze().numpy(),  # Convert the waveform to a numpy array
+        "array": waveform.squeeze().numpy(),  # convert the waveform to a numpy array
         "sampling_rate": new_sample_rate
     }
     return batch
 
+
 def make_dataset(data_dir: str):
     """
-    Creates a huggingface dataset given an input directory structred as follows:
+    Creates a huggingface dataset given an input directory structured as follows:
     data/ 
         audio_1.mp3
         audio_2.mp3 
         ...
         transcript.json
     Args:
-        input_dir (str, optional): Path of the input directory. Defaults to 'data'
+        data_dir (str): Path to the input directory
     Returns:
         dataset (Dataset): A huggingface dataset object created from the (audio, text transcription) data in input_dir
     """
@@ -113,35 +102,27 @@ def make_dataset(data_dir: str):
     dataset = Dataset.from_dict(dataset_dict)
 
     # convert the audio to the correct format for Whisper transcription
-    dataset = dataset.map(convert_audio_for_whisper)
+    dataset = dataset.map(resample_audio)
     return dataset
 
-# CHANGE ARGS DOCS
+
 def segment_audio_from_transcript(transcript_path: str, mp3_path: str, output_dir="data"):
     """
-    Splits audio into segments given an MP3 file and a transcript with start and end timestamps.
-    Saves a `transcriptions.json` file with data for each segment in the following format:
-    {
-        "audio": "audio_path.mp3",
-        "text": "Ground truth text for audio_path.mp3"
-    }
+    Splits audio into segments given an MP3 file and a transcript.json file with start and end timestamps.
+    Adds an "audio" key to each entry with the path to the mp3 file. 
     Args:
-        transcript (list): A list of tuples where each tuple contains (start, end, text):
-            - start (int): Start timestamp in milliseconds
-            - end (int): End timestamp in milliseconds
-            - text (str): The ground truth transcription for the audio segment
-        mp3_path (str, optional): Path to the MP3 audio file to be split. Defaults to 'downloads/audio.mp3'
+        transcript_path (str): Path to transcript.json file
+        mp3_path (str): Path to the MP3 audio file to be split
         output_dir (str, optional): Directory where the audio segments and transcriptions will be saved. Defaults to 'data'
     Returns:
         None
     """
-
     with open(transcript_path, "r") as file:
         transcript = json.load(file)
 
     audio = AudioSegment.from_mp3(mp3_path)
-    os.makedirs(output_dir, exist_ok=True)
     total_segments = len(transcript)
+    os.makedirs(output_dir, exist_ok=True)
 
     # progress bar for audio splitting
     with tqdm(total=total_segments, desc="Segmenting Audio", unit="segment") as pbar:
@@ -155,18 +136,37 @@ def segment_audio_from_transcript(transcript_path: str, mp3_path: str, output_di
             segment = audio[segment_data["start"]:segment_data["end"]]
             segment.export(segment_path, format="mp3")
             
-            # append json data
+            # add audio key
             segment_data["audio"] = segment_filename
 
             # update progress bar
             pbar.set_postfix_str(f"Text: {segment_data['text'][:75].ljust(75)}")
             pbar.update(1)  
 
-    # write transcriptions.json to file
+    # save transcript.json
     with open(transcript_path, 'w') as file:
         json.dump(transcript, file, indent=2)
 
     print(f"[audio_splitter] Transcriptions have been successfully written to {transcript_path}")
+
+def filter_empty_references(references, predictions):
+    """
+    Filters out empty references (blocks of silence) from transcript.
+    Args:
+        references (list): List of references (i.e. ground truth transcriptions)
+        predictions (list): List of Whisper transcriptions
+    Returns:
+        filtered_references (list): List of references with empty references removed
+        filtered_predictions (list): List of Whisper transcriptions with corresponding 
+        empty references removed
+    """
+    filtered_references, filtered_predictions = [], []
+    for ref, pred in zip(references, predictions):
+        # only include non-empty references
+        if ref.strip():  
+            filtered_references.append(ref)
+            filtered_predictions.append(pred)
+    return filtered_references, filtered_predictions
 
 if __name__ == "__main__":
     segment_audio_from_transcript()
--- yt_whisper_pipeline.py
+++ yt_whisper_pipeline.py
@@ -1,12 +1,10 @@
 import shutil
 import os
-import torch
 
-from utilities import segment_audio_from_transcript, make_dataset, save_transcript
-from yt_downloader import download_mp3, download_transcript
-from transformers import WhisperProcessor, WhisperForConditionalGeneration
+from utilities import make_dataset, save_transcript, filter_empty_references
+from whisper_utilities import get_whisper_transcription
+from yt_downloader import download_and_preprocess_yt
 from evaluate import load
-from datasets import Dataset
 
 """
 URLs:
@@ -19,13 +17,13 @@ Manually transcribed captions:
 
 # configuration settings
 URL = "https://www.youtube.com/watch?v=4WEQtgnBu0I"
-EXPERIMENT_TITLE = "test1"
+TITLE = "elevator_pitch"
 
 MODEL_TYPE = "openai/whisper-base"
 DATASET_DIR = "datasets"
 
-TRANSCRIPT_FILENAME = EXPERIMENT_TITLE + ".txt"
-DATASET_NAME = EXPERIMENT_TITLE + "_"+ MODEL_TYPE.replace("/", "-") + "_ds"
+TRANSCRIPT_FILENAME = TITLE + ".txt"
+DATASET_NAME = TITLE + "_"+ MODEL_TYPE.replace("/", "-") + "_ds"
 
 REMOVE_EMPTY_REFERENCES = True
 SAVE_TRANSCRIBED_DS = False
@@ -34,136 +32,34 @@ SAVE_TRANSCRIBED_DS = False
 Garbage collector should remain on as transcription will break if previous audio remains in 'data/' output_dir!
 Bearing this in mind however it can be turned off in order to inspect audio segmentation.
 """
-GARBAGE_COLLECTOR = False 
-
-def preprocess_video(url: str, download_dir="downloads", output_dir="data"):
-    """
-    Downloads mp3 audio and vtt transcript from youtube URL and splits data into 
-    audio segments. Creates a transcriptions.json file containing corresponding 
-    (audio segment, transcript segment) info needed for creating a huggingface dataset.
-    
-    Args:
-        url (str): Youtube video URL
-        audio_dir (str, optional): Directory to save downloaded audio. Defaults to 'downloads'.
-        data_dir (str, optional: Directory to save segmented audio data. Defaults to 'data'.
-    
-    Returns:
-        None
-    """
-
-    download_mp3(url, download_dir=download_dir)
-    download_transcript(url, download_dir=download_dir, output_dir=output_dir)
-    mp3_path = os.path.join(download_dir, "audio.mp3")
-    transcript_path = os.path.join(output_dir, "transcript.json")
-    segment_audio_from_transcript(transcript_path, mp3_path=mp3_path, output_dir=output_dir)
-
-
-def batch_inference(batch, model, processor, device):
-    """
-    Performs inference on a batch of data. 
-    
-    Args:
-        batch (dict): A dictionary containing the audio and text data.
-
-    Returns:
-        batch (dict): The original batch with added "reference", "transcription", 
-        and "prediction" keys.
-    """
-
-    # tokenize ground truth text
-    batch["reference"] = processor.tokenizer._normalize(batch['text'])
-
-    # make model predictions
-    audio = batch["audio"]
-    input_features = processor(audio["array"], sampling_rate=audio["sampling_rate"], return_tensors="pt").input_features
-
-    # no_grad: no gradient calculations i.e. no training backward pass, only inference
-    with torch.no_grad(): 
-        predicted_ids = model.generate(input_features.to(device))[0]
-    
-    # tokenize whisper transcription and add to batch
-    batch["transcription"] = processor.decode(predicted_ids, skip_special_tokens=True)
-    batch["prediction"] = processor.tokenizer._normalize(batch["transcription"])
-    return batch
-
-
-def get_whisper_transcription(ds: Dataset):
-    """
-    Gets Whisper transcriptions of audio from dataset.
-    
-    Args:
-        ds (Dataset): A dataset containing audio and ground truth transcription data
-
-    Returns:
-        transcribed_ds (Dataset): The original dataset with added "reference", "prediction"
-        and "tanscription" keys containing Whisper transcriptions
-    """
-
-    # load GPU if available
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    print(f"[pipeline] Using {device}: {torch.cuda.get_device_name(0)}")
-   
-    # load model and processor
-    processor = WhisperProcessor.from_pretrained(MODEL_TYPE)
-    model = WhisperForConditionalGeneration.from_pretrained(MODEL_TYPE).to(device)
-    model.config.forced_decoder_ids = processor.get_decoder_prompt_ids(language="english", task="transcribe")
-
-    # make transcription inference
-    transcribed_ds = ds.map(lambda batch: batch_inference(batch, model, processor, device))
-    return transcribed_ds
-
-
-def filter_empty_references(references, predictions):
-    """
-    Filters out empty references (blocks of silence) from transcript.
-    
-    Args:
-        references (list): List of references (i.e. ground truth transcriptions)
-        predictions (list): List of Whisper transcriptions
-
-    Returns:
-        filtered_references (list): List of references with empty references removed
-        filtered_predictions (list): List of Whisper transcriptions with corresponding 
-        empty references removed
-    """
-
-    filtered_references, filtered_predictions = [], []
-    for ref, pred in zip(references, predictions):
-        # only include non-empty references
-        if ref.strip():  
-            filtered_references.append(ref)
-            filtered_predictions.append(pred)
-    return filtered_references, filtered_predictions
-
-
-def pipeline():
-    return 0
+GARBAGE_COLLECTOR = True 
 
 if __name__ == "__main__":
     """
-    evaluate_yt_transcription_pipeline.py 
+    yt_whisper_pipeline:
         1. Downloads the audio and transcript from a given youtube video URL
         2. Splits audio and transcript into segments and saves as a huggingface dataset in DATASET_DIR\DATASET_NAME
-        3. Uses whisper for english transcription on dataset
+        3. Uses whisper for English transcription on dataset
         4. Calculates Word Error Rate of whisper transcription against original youtube transcript 
     """
     print(f"[pipeline] Initialised. Preparing to download youtube data.")
-    if GARBAGE_COLLECTOR == False: print(f"[WARNING] Garbage Collection is turned off. Errors will be caused if data/ and downloads/ are not empty!")
+
+    # garbage collection
+    if GARBAGE_COLLECTOR: 
+        shutil.rmtree("downloads") 
+        shutil.rmtree("data")
+    else:
+        print(f"[WARNING] Garbage Collection is turned off. Errors will be caused if data/ and downloads/ are not empty!")
 
     # download and segment youtube audio and transcript
-    preprocess_video(URL)
+    download_and_preprocess_yt(URL)
 
     # save as huggingface dataset
     ds = make_dataset(data_dir="data")
     dataset_path = os.path.join(DATASET_DIR, DATASET_NAME)
     ds.save_to_disk(dataset_path=dataset_path)
 
-    # delete downloads and data
-    if GARBAGE_COLLECTOR:
-        shutil.rmtree("downloads") 
-        shutil.rmtree("data")
-
-    # save whisper generated transcript to file
+    # generate whisper transcript and save to to file
     transcribed_ds = get_whisper_transcription(ds)
     save_transcript(ds=transcribed_ds, transcript_filename=TRANSCRIPT_FILENAME)
 
@@ -181,5 +77,5 @@ if __name__ == "__main__":
 
     # calculate final WER
     wer_function = load("wer")
-    wer_score = wer_function.compute(references=references, predictions=predictions)
-    print(f"[pipeline] Word Error Rate (WER) of Whisper transcriptions against youtube subtitles: {100 * wer_score}%")
+    wer_score = 100 * wer_function.compute(references=references, predictions=predictions)
+    print(f"[pipeline] Word Error Rate (WER) of Whisper transcriptions against youtube subtitles: {wer_score:.3f}%")
