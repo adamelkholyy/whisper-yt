7bb25a6f15e2ac158aeeb4c9f02c4b8238e29bfb
adamelkholyy <78866717+adamelkholyy@users.noreply.github.com>
2024-09-24 14:07:23 +0100

    update main.py

--- main.py
+++ main.py
@@ -6,25 +6,17 @@ from whisper_yt.whisper_utilities import get_whisper_transcription
 from whisper_yt.yt_downloader import download_and_preprocess_yt
 from evaluate import load
 
-"""
-URLs:
-Auto-generated captions:
-    40 second elevator_pitch: https://www.youtube.com/watch?v=4WEQtgnBu0I
-    50 minute Beck Institute CBT roleplay: https://www.youtube.com/watch?v=ac5Jct33oUU
-Manually transcribed captions: 
-    3 minute tutorial video: https://www.youtube.com/watch?v=VatNBZh66Po
-"""
+# useful example URLs
+elevator_pitch_url = "https://www.youtube.com/watch?v=4WEQtgnBu0I" # 40 seconds, elevator pitch
+job_interview_url = "https://www.youtube.com/watch?v=naIkpQ_cIt0"  # 2 minutes, two person job interview 
+tutorial_video_url = "https://www.youtube.com/watch?v=VatNBZh66Po" # 3 minutes, manually transcribed subtitles
 
 # configuration settings
-URL = "https://www.youtube.com/watch?v=4WEQtgnBu0I"
-TITLE = "elevator_test"
+URL = job_interview_url
+TITLE = "job_interview"
 
 MODEL_TYPE = "openai/whisper-base"
 DATASET_DIR = "datasets"
-
-TRANSCRIPT_FILENAME = TITLE + ".txt"
-DATASET_NAME = TITLE + "_"+ MODEL_TYPE.replace("/", "-") + "_ds"
-
 REMOVE_EMPTY_REFERENCES = True
 SAVE_TRANSCRIBED_DS = False
 
@@ -34,9 +26,13 @@ Bearing this in mind however it can be turned off in order to inspect audio segm
 """
 GARBAGE_COLLECTOR = True 
 
+# create filenames
+TRANSCRIPT_FILENAME = TITLE + ".txt"
+DATASET_NAME = TITLE + "_"+ MODEL_TYPE.replace("/", "-") + "_ds"
+
 if __name__ == "__main__":
     """
-    yt_whisper_pipeline:
+    Example pipeline:
         1. Downloads the audio and transcript from a given youtube video URL
         2. Splits audio and transcript into segments and saves as a huggingface dataset in DATASET_DIR\\DATASET_NAME
         3. Uses whisper for English transcription on dataset
@@ -46,8 +42,8 @@ if __name__ == "__main__":
 
     # garbage collection
     if GARBAGE_COLLECTOR: 
-        shutil.rmtree("downloads") 
-        shutil.rmtree("data")
+        if os.path.isdir('downloads'): shutil.rmtree("downloads") 
+        if os.path.isdir('data'): shutil.rmtree("data")
         print(f"[pipeline] Garbage collector: downloads/ and data/ successfully deleted")
     else:
         print(f"[pipeline] WARNING: Garbage Collection is turned off. Errors will be caused if data/ and downloads/ are not empty!")
